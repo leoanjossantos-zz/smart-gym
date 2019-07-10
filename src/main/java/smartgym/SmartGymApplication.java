b2d99d8cfdd3f89db8d7ce8f4c17145fe7e37ff9
Leandro Santos <leoanjossantos@gmail.com>
2019-07-10 16:20:33 -0300

    Adding Spring Boot dependencies

---
+++ src/main/java/smartgym/SmartGymApplication.java
@@ -0,0 +1,22 @@
+package smartgym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class SmartGymApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartGymApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplateBuilder().build();
+    }
+
+
+}
